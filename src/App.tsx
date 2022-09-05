c687bcdb0c3e62cf7e703d2590280d667eca05f4
Solaucas <matheusroot2608@gmail.com>
2022-09-05 18:17:06 -0300

    teste again

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,11 @@ import AOS from 'aos';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
-import { AppRoutes } from './Routes';
 import { GlobalDiv } from './Styles/GlobalDiv';
 
 import 'aos/dist/aos.css';
 import ContextProvider from './Providers/ContextProvider';
+import Home from './Pages/Home';
 
 function App() {
   AOS.init();
@@ -15,7 +15,7 @@ function App() {
   return (
     <GlobalDiv>
       <ContextProvider>
-        <AppRoutes />
+        <Home />
       </ContextProvider>
       <ToastContainer theme="colored" />
     </GlobalDiv>
